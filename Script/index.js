6ecc6644d62b94e15e44e2787a38054e245110f6
bayjidalom <bayjidalom1005@gmail.com>
2026-03-09 12:49:40 +0600

    Issues load & display

--- Script/index.js
+++ Script/index.js
@@ -1,20 +1,16 @@
-// console.log("Hello World!")
+// console.log("Connected")
+
 document.getElementById("sign-btn").addEventListener("click",()=>{
 
-    // 1-get the username & pass from the input's
     const usernameInput = document.getElementById("username-input");
     const username = usernameInput.value;
-    // console.log(username);
 
     const passInput = document.getElementById("password-input");
     const password = passInput.value;
-    // console.log(password)
 
-
-    // 2- match username and password
     if(username === "admin" && password === "admin123"){
         alert("Login Successful");
-        window.location.assign("home.html");
+        window.location.assign("/home.html");
     }else{
         alert("Login Failed");
         return;
